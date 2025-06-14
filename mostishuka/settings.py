6185c32bdf162f03d8d265c8848cbe72c8a01698
Suren <ananian.suren96@gmail.com>
2025-06-14 11:19:05 +0400

    Upade 06.14.2025-v2

--- mostishuka/settings.py
+++ mostishuka/settings.py
@@ -117,7 +117,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
+
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # если у тебя есть своя папка static/
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"  # куда соберётся статика
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
