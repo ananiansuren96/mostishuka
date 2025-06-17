783c4140fcd0c1923e645457e765889a09028629
Suren <ananian.suren96@gmail.com>
2025-06-17 17:50:31 +0400

    Добавил папку media

--- mostishuka/urls.py
+++ mostishuka/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', include('listings.urls')),  # Все пути приложения здесь
 ]
 
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
